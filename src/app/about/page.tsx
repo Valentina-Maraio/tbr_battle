ff2d34ae75ab7aa6f0ca66d914f4183d237126cd
Valentina Maraio <valentina.maraio89@gmail.com>
2024-12-28 12:11:20 +0100

    first cypress text on two pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About Us</h1>
+            <p>This is the About page of our TBR Battle Dashboard</p>
+            <a href="/">Go to Home Page</a>
+        </main>
+    )
+}
