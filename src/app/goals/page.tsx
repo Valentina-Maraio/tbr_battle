8ca530504341cbb28788401792595429c375be9a
Valentina Maraio <valentina.maraio89@gmail.com>
2025-01-05 20:30:31 +0100

    reading goals wip

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,11 +1,37 @@
+'use client'
+
 import HomePage from "../homepage/page";
+import { useFavorite } from "../context/ReadingContext";
 
 export default function GoalsPage() {
+  const { favoriteBooks, removeFavorite } = useFavorite(); 
+
   return (
     <HomePage>
       <div>
         <h1 className="text-2xl font-bold">Goals</h1>
         <p>Who's reading more books than you?</p>
+        {/* Display reading books */}
+        {favoriteBooks.length > 0 ? (
+          <div>
+            <h2>Currently Reading Books:</h2>
+            <ul>
+              {favoriteBooks.map((book) => (
+                <li key={book.Book_Id}>
+                  {book.Title} by {book.Author}
+                  <button
+                    onClick={() => removeFavorite(book.Book_Id)}
+                    className="ml-2 bg-red-500 text-white px-2 py-1 rounded"
+                  >
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No books are being read right now.</p>
+        )}
       </div>
     </HomePage>
   );
